53cb52a491e452f1fa3d045eef16290343b86692
DragonistYJ <1121498469@qq.com>
2020-06-09 20:52:13 +0800

    2020.6.9 ui

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <el-container direction="vertical">
+        <Header/>
+        <MenuUser/>
+        <Footer/>
+    </el-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Header from "@/components/Header";
+    import MenuUser from "@/components/MenuUser";
+    import Footer from "@/components/Footer";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Header,
+            MenuUser,
+            Footer
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .el-container .el-header {
+        padding: 0;
+    }
 </style>
---
+++ webui/src/components/MenuUser.vue
@@ -0,0 +1,43 @@
+<template>
+    <el-row>
+        <el-col :offset="4" :span="2">
+            <p>首页</p>
+        </el-col>
+        <el-col :span="2">
+            <p>托福人</p>
+        </el-col >
+        <el-col :span="2">
+            <p>公开课</p>
+        </el-col>
+        <el-col :span="2">
+            <p>群聊组</p>
+        </el-col>
+        <el-col :span="2">
+            <p>福利城堡</p>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+    export default {
+        name: "MenuUser"
+    }
+</script>
+
+<style scoped>
+    .el-row {
+        background-color: #68BF4A;
+    }
+
+    p {
+        font-size: 18px;
+        color: white;
+        margin: 0;
+        padding: 1em;
+        text-align: center;
+    }
+
+    p:hover {
+        background-color: #063;
+    }
+</style>
