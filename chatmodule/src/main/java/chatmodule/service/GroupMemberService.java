7f62637931819ce1c854862ab5d38981fafe7c62
gofreelee <1979432070@qq.com>
2020-06-12 17:33:58 +0800

    chatmodule v1.0

--- chatmodule/src/main/java/chatmodule/service/GroupMemberService.java
+++ chatmodule/src/main/java/chatmodule/service/GroupMemberService.java
@@ -1,14 +1,26 @@
 package chatmodule.service;
 
+import chatmodule.bean.Group;
+import chatmodule.bean.GroupInfoQuery;
 import chatmodule.bean.GroupMember;
+import chatmodule.bean.MemberQuery;
+import chatmodule.mapper.GroupDao;
 import chatmodule.mapper.GroupMemberDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.swing.plaf.metal.MetalMenuBarUI;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class GroupMemberService {
     @Autowired
     GroupMemberDao groupMemberDao;
+
+    @Autowired
+    GroupDao groupDao;
+
     public int addMember(GroupMember groupMember){
        return  groupMemberDao.addGroupMember(groupMember);
     }
@@ -16,5 +28,29 @@ public class GroupMemberService {
     public int deleteMember(String gmbUsername){
         return groupMemberDao.deleteMember(gmbUsername);
     }
+
+    public GroupInfoQuery queryGroupInfo(int grpId){
+        GroupInfoQuery groupInfoQuery = new GroupInfoQuery();
+        Group group = groupDao.selectByGrpID(grpId);
+        int membersNum = groupMemberDao.calculateAllMemberNum(grpId);
+        List<MemberQuery>  allMembers = groupMemberDao.selectMemberInfo(grpId);
+        List<MemberQuery>  managers = new ArrayList<>(), members = new ArrayList<>();
+        for(MemberQuery member: allMembers){
+            if(member.getUserType().equals("manager"))
+                managers.add(member);
+            else
+                members.add(member);
+        }
+        groupInfoQuery.setBaseInfo(group);
+        groupInfoQuery.setGroupMembers(membersNum);
+        groupInfoQuery.setManagers(managers);
+        groupInfoQuery.setMembers(members);
+        return groupInfoQuery;
+    }
+
+    public List<MemberQuery> queryGroupByType(int grpId, String gmpType){
+        return groupMemberDao.selectMemberByType(grpId, gmpType);
+    }
+
 
 }
--- chatmodule/src/main/java/chatmodule/service/GroupMessageService.java
+++ chatmodule/src/main/java/chatmodule/service/GroupMessageService.java
@@ -5,6 +5,8 @@ import chatmodule.mapper.GroupMessageDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class GroupMessageService {
     @Autowired
@@ -16,5 +18,13 @@ public class GroupMessageService {
     public int removeMessage(int gmsId)
     {
         return groupMessageDao.deleteMessage(gmsId);
+    }
+
+    public int calculateMessageNum(int grpId){
+        return groupMessageDao.calculateMessageNum(grpId);
+    }
+
+    public List<GroupMessage> selectMessageLimitBy(int grpId, int start, int end){
+        return groupMessageDao.selectMessageLimitBy(grpId, start, end);
     }
 }
