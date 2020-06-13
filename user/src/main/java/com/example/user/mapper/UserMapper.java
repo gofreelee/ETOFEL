f42bb233a82955cad168a479a1088b884f4a882e
src <643036264@qq.com>
2020-06-13 15:05:59 +0800

    usermodule

---
+++ user/src/main/java/com/example/user/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.example.user.mapper;
+
+import com.example.user.bean.User;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserMapper {
+    // 获取用户信息
+    User getUserByUsername(String usr_username);
+    //修改用户信息
+    void updateUser(User user);
+    //修改用户密码
+    void updatePwd(@Param("usr_username") String usr_username, @Param("pwd") String pwd);
+    //上传用户头像
+    String updatePhoto(@Param("usr_username") String usr_username,@Param("usr_portrait") String usr_portrait);
+}
