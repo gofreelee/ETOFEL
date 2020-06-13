3436392e9a1696605dee9682ed509edb889b2ec0
gofreelee <1979432070@qq.com>
2020-06-13 16:43:50 +0800

    改冲突

--- coursemodule/src/main/java/sichuan/umbrella/chenmm/mapper/CourseMapper.java
+++ coursemodule/src/main/java/sichuan/umbrella/chenmm/mapper/CourseMapper.java
@@ -1,42 +1,4 @@
-<<<<<<< HEAD
-package sichuan.umbrella.chenmm.mapper;
 
-import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Component;
-import sichuan.umbrella.chenmm.bean.Course;
-
-import java.util.List;
-
-@Component
-public interface CourseMapper {
-    /**
-     * 选择课程类型（公益、热门、名师）
-     * @param cosCategory 课程类型：听力、写作……
-     * @return 满足条件的课程列表
-     */
-    List<Course> selectActivityIsPublic(@Param("cos_category") String cosCategory);
-    List<Course> selectActivityIsHot(@Param("cos_category") String cosCategory);
-    List<Course> selectActivityIsFamousTeacher(@Param("cos_category") String cosCategory);
-
-    /**
-     * 发布课程--基本信息
-     * @param course 课程
-     */
-    void insertCourseBasicInfo(@Param("course") Course course);
-
-    /**
-     * 课程列表页按照时间排序
-     * @param cosStartDate 课程开始日期2020-06-13
-     * @param cosStartTime 课程开始上课时间10:30
-     * @return 排序后的课程列表
-     */
-    List<Course> selectCourseByTime(@Param("cos_start_date") String cosStartDate, @Param("cos_start_time") String cosStartTime);
-
-    List<Course> selectCourseByUser(@Param("usr_username") String usr_username);
-    List<Course> selectCourseByTeacher(@Param("tch_username") String tch_username);
-}
-
-=======
 package sichuan.umbrella.chenmm.mapper;
 
 import org.apache.ibatis.annotations.Param;
@@ -104,4 +66,3 @@ public interface CourseMapper {
     Course selectCourseById(@Param("cosId") int cosId);
 }
 
->>>>>>> 7867d3d8b1b402b7af7ac7df7e15a491069de3c7
--- coursemodule/src/main/java/sichuan/umbrella/chenmm/service/CourseService.java
+++ coursemodule/src/main/java/sichuan/umbrella/chenmm/service/CourseService.java
@@ -1,79 +1,4 @@
-<<<<<<< HEAD
-package sichuan.umbrella.chenmm.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import sichuan.umbrella.chenmm.bean.Course;
-import sichuan.umbrella.chenmm.mapper.CourseMapper;
-
-import java.util.List;
-
-@Service
-public class CourseService {
-    private CourseMapper courseMapper;
-
-    @Autowired
-    public void setCourseMapper(CourseMapper courseMapper) {
-        this.courseMapper = courseMapper;
-    }
-
-    /**
-     * 课程选择为公益课程(免费课程)
-     * @param cosCategory 课程类型：听力、写作……
-     * @return 满足选项的课程列表
-     */
-    public List<Course> selectActivityIsPublic(String cosCategory) {
-        return courseMapper.selectActivityIsPublic(cosCategory);
-    }
-
-    /**
-     * 课程选择为热门课程
-     * @param cosCategory 课程种类
-     * @return 满足选项的课程列表
-     */
-    public List<Course> selectActivityIsHot(String cosCategory) {
-        return courseMapper.selectActivityIsHot(cosCategory);
-    }
-
-    /**
-     * 课程选择为名师精品课程
-     * @param cosCategory 课程种类
-     * @return 满足选项的课程列表
-     */
-    public List<Course> selectActivityIsFamousTeacher(String cosCategory) {
-        return courseMapper.selectActivityIsFamousTeacher(cosCategory);
-    }
-
-    /**
-     * 发布课程--基本信息
-     * @param course 课程类
-     * @return boolean类型
-     */
-    public boolean insertCourseBasicInfo(Course course) {
-        courseMapper.insertCourseBasicInfo(course);
-        return course.noNullValue();
-    }
-
-    /**
-     * 按时间从早到晚给课程排序
-     * @param cosStartDate 开始日期2020-06-13
-     * @param cosStartTime 开始时间10:30
-     * @return 排序后的课程列表
-     */
-    public List<Course> selectCourseByTime(String cosStartDate, String cosStartTime) {
-        return courseMapper.selectCourseByTime("cosStartDate", "cosStartTime");
-    }
-
-    /*查询个人的课程*/
-    public List<Course> selectCourseByUser(String usr_username){
-        return courseMapper.selectCourseByUser(usr_username);
-    }
-    /*查询老师的课程*/
-    public List<Course> selectCourseByTeacher(String tch_username){
-        return courseMapper.selectCourseByTeacher(tch_username);
-    }
-}
-=======
 package sichuan.umbrella.chenmm.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -184,4 +109,3 @@ public class CourseService {
         return course;
     }
 }
->>>>>>> 7867d3d8b1b402b7af7ac7df7e15a491069de3c7
